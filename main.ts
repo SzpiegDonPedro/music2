6f56fb896765db68e19316e005ff22b7af79b17b
SzpiegDonPedro <166158045+SzpiegDonPedro@users.noreply.github.com>
2024-11-09 09:38:30 +0100

    Aktualizacja main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 buttonClicks.onButtonDoubleClicked(buttonClicks.AorB.A, function () {
     media.sendCode(media.keys(media._MediaKey.previous))
+    media.sendCode(media.keys(media._MediaKey.vol_up))
     basic.showLeds(`
         # . . # .
         # # . # .
@@ -26,18 +27,27 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     media.sendCode(media.keys(media._MediaKey.playPause))
     if (PLAY == false) {
         PLAY = true
+        basic.showLeds(`
+            . # . . .
+            . # # . .
+            . # # # .
+            . # # . .
+            . # . . .
+            `)
+        basic.pause(500)
+        basic.clearScreen()
     } else {
         PLAY = false
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . # . # .
+            . # . # .
+            . # . # .
+            `)
+        basic.pause(500)
+        basic.clearScreen()
     }
-    basic.showLeds(`
-        . # . . .
-        . # # . .
-        . # # # .
-        . # # . .
-        . # . . .
-        `)
-    basic.pause(500)
-    basic.clearScreen()
 })
 input.onButtonPressed(Button.AB, function () {
     media.sendCode(media.keys(media._MediaKey.mute))
@@ -65,6 +75,7 @@ input.onButtonPressed(Button.B, function () {
 })
 buttonClicks.onButtonDoubleClicked(buttonClicks.AorB.B, function () {
     media.sendCode(media.keys(media._MediaKey.next))
+    media.sendCode(media.keys(media._MediaKey.vol_down))
     basic.showLeds(`
         . # . . #
         . # . # #
@@ -75,24 +86,12 @@ buttonClicks.onButtonDoubleClicked(buttonClicks.AorB.B, function () {
     basic.pause(500)
     basic.clearScreen()
 })
-input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
-    media.sendCode(media.keys(media._MediaKey.stop))
-    basic.showLeds(`
-        # . . . #
-        . # # # .
-        . # . # .
-        . # # # .
-        # . . . #
-        `)
-    basic.pause(500)
-    basic.clearScreen()
-})
 let PLAY = false
-basic.showString("MUSIC 2")
 media.startMediaService()
 music.play(music.stringPlayable("C5 B G B A F E D ", 227), music.PlaybackMode.InBackground)
-basic.showString("SELECT STOP")
-basic.pause(500)
+basic.showString("MUSIC 2")
+basic.showString("S-S")
+basic.pause(1000)
 PLAY = false
 basic.forever(function () {
     if (PLAY == true) {
